67cd471bd327e1d1ca34b091c44feee9d1a239df
Magnus Bomholt <magbom123@gmail.com>
2023-09-05 10:27:35 +0200

    mit dokument

---
+++ Magnus/script.js
@@ -0,0 +1,13 @@
+// var myVar = 12;
+// myVar ='Hello'
+//alert(myVar);
+// alert(typeof(myVar)); 
+
+// <input id="txt" type="text"/>
+// <button onclick="alert(document.getElementById('txt').value);">Alert Me</button>
+var txt = 2;
+var tzt = 3;
+var samlet = tzt + txt;
+
+document.write(samlet);
+
